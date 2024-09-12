b46d1d8549321b6d1c87b6892edb69d7ea31a201
Lalit Budhathoki <lalitbudhathoki03@gmail.com>
2024-09-12 16:31:11 +0545

    day 3

---
+++ day3.py
@@ -0,0 +1,13 @@
+#learning tkinter 
+# from tkinter import *
+# saurav_root=Tk()
+# saurav_root.mainloop()
+from tkinter import *
+root=Tk()
+root.geometry("600x400")
+root.minsize(600,400)
+root.maxsize(600,399)
+label1=Label(text="saurav is my name")
+
+label1.pack()
+root.mainloop()
